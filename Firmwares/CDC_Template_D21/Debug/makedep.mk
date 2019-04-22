6cc28df1dc11d8d7723a0306b44806142307b5ac
AloyseTech <meyertheopro@gmail.com>
2019-04-22 18:42:24 +0200

    add formware solution

---
+++ Firmwares/CDC_Template_D21/Debug/makedep.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Device_Startup\startup_samd21.c
+
+main.c
+
+uart.c
+
+usb.c
+
+usb_cdc.c
+
+usb_descriptors.c
+
+usb_std.c
+
